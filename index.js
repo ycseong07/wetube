e27661e1bdc0c4f0be2a38dfb0423f4ba3d77602
syc <ycseong07@gmail.com>
2019-09-04 17:57:26 +0900

    basic ~2.4

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("hi");
+const express = require("express"); //import express .. 모든걸 설치 후 require or import하면 됨.
+const app = express(); //app변수를 선언해서 express를 실행
+
+const PORT = 4000;
+
+const handleListening = () => {
+  console.log(`Listening on: http://localhost:${PORT}`);
+};
+
+const handleHome = (req, res) => {
+  console.log(req); // req라는 obj가 나열될거임.
+  res.send("hello from home");
+};
+
+const handleProfile = (req, res) => {
+  res.send("You are on my profile");
+};
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
