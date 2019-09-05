0f47ca14cf114e27d794f4a367fe1b6f5e3d018e
syc <ycseong07@gmail.com>
2019-09-05 15:38:23 +0900

    ~2.7

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-const express = require("express"); //import express .. 모든걸 설치 후 require or import하면 됨.
+//import express .. 모든걸 설치 후 require or import하면 됨.
+// const express = require("express"); 아래 코드와 같음.
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+
 const app = express(); //app변수를 선언해서 express를 실행
 
 const PORT = 4000;
 
-const handleListening = () => {
+const handleListening = () =>
   console.log(`Listening on: http://localhost:${PORT}`);
-};
 
-const handleHome = (req, res) => {
-  console.log(req); // req라는 obj가 나열될거임.
-  res.send("hello from home");
-};
+const handleHome = (req, res) => res.send("hello from home");
 
-const handleProfile = (req, res) => {
-  res.send("You are on my profile");
-};
+const handleProfile = (req, res) => res.send("You are on my profile");
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: ture }));
+app.use(morgan("dev"));
+app.use(helmet());
 
 app.get("/", handleHome);
 
