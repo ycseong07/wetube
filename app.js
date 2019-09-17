c8c9d9af868b57e58bea302aaee62c73ed65aeed
ycseong <ycseong07@gmail.com>
2019-09-17 17:46:40 +0900

    4강까지

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(helmet());
 app.set("view engine", "pug");
 // 누군가 /uploads로가면 'uploads'라는 디렉토리로 들어가진다
 app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
